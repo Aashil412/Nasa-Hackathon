4bd150117c2ca0651ca81138956a72a56e2038cb
Omr50 <omrab4@gmail.com>
2023-10-07 22:14:32 -0400

    Home Page

---
+++ Client/src/Components/AppStructure.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { ReactNode, useState } from 'react'
+import NavigationBar from './NavBar/NavBarComponent';
+import WelcomeComponent from './Welcome/Welcome';
+import Profile from './Profile/Profile';
+
+interface elementProp {
+}
+
+const AppStructure = () => {
+
+    return (
+        <div className=''>
+        {/* <AuthProvider> */}
+            <BrowserRouter>
+            {/* <AuthProvider> */}
+                <NavigationBar/>
+                <Routes>
+                        <Route path='' element={<WelcomeComponent/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        {/* <Route path='/signup' element={<SignupComponent changeComp={changeComponent} closeForm={closeComp}/>}/> */}
+                </Routes>
+                {/* <FooterComponent/> */}
+            {/* </AuthProvider> */}
+            </BrowserRouter>
+        {/* </AuthProvider> */}
+        </div>
+    )
+}
+
+export default AppStructure;
