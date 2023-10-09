a887b0af29d58c292e664c78ef4b8ac6f813790e
Aashi <aashilahmad@gmail.com>
2023-10-08 23:41:57 -0400

    Commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AppStructure from './Components/AppStructure'
-import CreateProject from './Components/CreateProject'
+import CreateProject from './Components/Project/CreateProject';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
